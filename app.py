18f433d88bc26d84f052116287c61727cd47d60a
Kenny Rowe <kennyrowe@gmail.com>
2020-08-03 21:22:44 -0700

    change names

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def hello_world():
     return render_template('index.html', total=total, quota=quota, result=result)        
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
